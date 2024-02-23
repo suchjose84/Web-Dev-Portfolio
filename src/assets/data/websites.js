43c10fb128db849d243022a7c7ee6a7deaf74cbf
suchjose84 <suchjose@live.com>
2024-02-23 17:45:59 +0800

    fix fix - image links

--- src/assets/data/websites.js
+++ src/assets/data/websites.js
@@ -4,25 +4,25 @@ export const websites = {
       {
         "title": "Spurbank",
         "description": "Blogging website in Angular",
-        "image": "/src/assets/website-images/spurbank.png",
+        "image": "../src/assets/website-images/spurbank.png",
         "platform": "Angular, Typescript, Bootstrap, CSS"
       },
       {
         "title": "PHP Motors",
         "description": "Full-stack website in PHP",
-        "image": "/src/assets/website-images/phpmotors.png",
+        "image": "../src/assets/website-images/phpmotors.png",
         "platform": "PHP, MySQL"
       },
       {
         "title": "Pokemon Pokedex",
         "description": "Pokemon Website",
-        "image": "/src/assets/website-images/pokedex.png",
+        "image": "../src/assets/website-images/pokedex.png",
         "platform": "Javascript"
       },
       {
         "title": "Panahon Weather",
         "description": "Weather Website",
-        "image": "/src/assets/website-images/panahon-weather.png",
+        "image": "../src/assets/website-images/panahon-weather.png",
         "platform": "HTML, CSS, Javascript"
       },
       {
