dd2ea55e3d08a9b28450f4c60e07b32e974c5ef3
suchjose84 <65042405+suchjose84@users.noreply.github.com>
2025-03-20 12:25:30 +0800

    updated portfolio

--- public/data/websites.js
+++ public/data/websites.js
@@ -1,4 +1,18 @@
 export const websites = [
+  
+  
+  {
+    "id": "06",
+    "title": "CK Web App - Under Construction",
+    "url": "https://master.d3vj0k0ftrw6er.amplifyapp.com/",
+    "description": "Food Supply App in Angular",
+    "about": "This is my personal project that I am currently working on. It is a food supply app that will be used by a local food supplier. The app will be used to manage the food supply chain, inventory, and sales. The app is built using Angular, Node.js, and MongoDB. The app is currently under construction and will be deployed soon. Best viewed on mobile devices for now.",
+    "image": {
+      "desktop": "/data/website-images/ckwebapp.png", 
+      "mobile": "/data/website-images/ckwebapp-mobile.png"
+    },
+    "platform": "Angular, Typescript, Bootstrap, CSS"
+  },
   {
     "id": "01",
     "title": "Spurbank",
@@ -16,7 +30,7 @@ export const websites = [
     "title": "PHP Motors",
     "url": "https://github.com/suchjose84/cse340",
     "description": "Full-stack website in PHP",
-    "about": "This is a full-stacked website using PHP in the front-end and mysql in the backend. Can only be deployed locally thru XAMPP.",
+    "about": "This is a full-stacked website using PHP in the front-end and mysql in the backend. Capable of handling user authentication, user input validation, and database management. Please download the repository and run it on your local server using XAMPP to view the website.",
     "image": {
       "desktop": "/data/website-images/phpmotors.png", 
       "mobile": "/data/website-images/phpmotors-mobile.png"
@@ -28,6 +42,7 @@ export const websites = [
     "title": "Pokemon Pokedex",
     "url": "https://suchjose84.github.io/wdd330_2/week14/index.html",
     "description": "Pokemon Website",
+    "about": "A simple website for collecting and viewing Pokemon data from an API. The website is built using HTML, CSS, and Javascript. The website is responsive and can be viewed on both desktop and mobile devices.",
     "image": {
       "desktop": "/data/website-images/pokedex.png", 
       "mobile": "/data/website-images/pokedex-mobile.png"
@@ -39,6 +54,7 @@ export const websites = [
     "title": "Panahon Weather",
     "url": "https://suchjose84.github.io/wdd230/lesson11/index.html",
     "description": "Weather Website",
+    "about": "A project that I made when I was still learning web development. It is a simple weather website that displays the current weather in a specific location. The website is built using HTML, CSS, and Javascript. The website is responsive and can be viewed on both desktop and mobile devices.",
     "image": {
       "desktop": "/data/website-images/panahon-weather.png", 
       "mobile": "/data/website-images/panahon-weather-mobile.png"
@@ -50,21 +66,11 @@ export const websites = [
     "title": "Scoots",
     "url": "https://suchjose84.github.io/wdd230/lesson14Scoots/index.html",
     "description": "Motorcycle Rental Website",
+    "about": "Another project that I made when I was still learning web development. It is a simple website for a motorcycle rental company. The website is built using HTML, CSS, and Javascript. The website is responsive and can be viewed on both desktop and mobile devices.",
     "image": {
       "desktop": "/data/website-images/scoots.png", 
       "mobile": "/data/website-images/scoots-mobile.png"
     },
     "platform": "HTML, CSS, Javascript"
   },
-  {
-    "id": "06",
-    "title": "CMS - Under Construction",
-    "url": "https://master.d2ymnt7dug4olp.amplifyapp.com/",
-    "description": "MEAN Stack Website",
-    "image": {
-      "desktop": "/data/website-images/scoots.png", 
-      "mobile": "/data/website-images/scoots-mobile.png"
-    },
-    "platform": "Angular, MongoDB"
-  }
 ];
